5da94dfa88f3ececdae95527c90e3a09ac6d033b
Angel Valkov <angel4o2003@abv.bg>
2026-02-13 09:35:43 +0200

    add websites

--- app_gui.py
+++ app_gui.py
@@ -14,10 +14,16 @@ PRESET_LINKS: dict[str, list[str]] = {
     "12 Punto": [
         "https://12punto.com.tr/gundem/chpden-meclis-baskani-kurtulmusun-anayasaya-aykiri-tesebbus-yorumuna-tepki-ilgileniyorsa-atalay-kararini-uygulasin-111020",
         "https://12punto.com.tr/gundem/bogazici-universitesinde-yurt-acilisi-sebebiyle-kampuse-giris-yasaklandi-cumhurbaskani-erdogan-gelecek-iddiasi-111016",
+        "https://12punto.com.tr/gundem/osman-gokcekten-iddia-ozgur-ozel-kursuyu-isgal-edin-dedi-111034",
+        "https://12punto.com.tr/ekonomi/istanbulda-toplu-tasimaya-zam-16-subattan-itibaren-111011",
     ],
     "Nova TV": [
         "https://nova.bg/news/view/2026/02/13/527075/%D1%81%D1%8A%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B5%D1%82%D0%BE-%D0%BD%D0%B0-15-%D0%B3%D0%BE%D0%B4%D0%B8%D1%88%D0%BD%D0%BE%D1%82%D0%BE-%D0%BC%D0%BE%D0%BC%D0%B8%D1%87%D0%B5-%D0%B2-%D0%BF%D0%B8%D1%80%D0%BE%D0%B3%D0%BE%D0%B2-%D0%B5-%D1%81%D1%82%D0%B0%D0%B1%D0%B8%D0%BB%D0%B8%D0%B7%D0%B8%D1%80%D0%B0%D0%BD%D0%BE-%D0%BD%D0%BE-%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%B0-%D1%82%D0%B5%D0%B6%D0%BA%D0%BE/",
         "https://nova.bg/news/view/2026/02/13/527078/%D0%B8%D0%B7%D0%BA%D1%83%D1%81%D1%82%D0%B2%D0%B5%D0%BD-%D0%B8%D0%BD%D1%82%D0%B5%D0%BB%D0%B5%D0%BA%D1%82-%D0%B5-%D1%81%D1%80%D0%B5%D0%B4-%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%B4%D0%B0%D1%82%D0%B8%D1%82%D0%B5-%D0%B7%D0%B0-%D0%B4%D0%B5%D0%BF%D1%83%D1%82%D0%B0%D1%82%D0%B8-%D0%B2-%D0%BA%D0%BE%D0%BB%D1%83%D0%BC%D0%B1%D0%B8%D1%8F/",
+        "https://nova.bg/news/view/2026/02/13/526049/%D1%88%D0%BE%D0%BA%D0%BE%D0%BB%D0%B0%D0%B4-%D0%B2%D0%B5%D1%80%D0%BC%D1%83%D1%82-%D0%B8-%D0%B2%D0%B8%D0%BD%D0%BE-%D0%B8%D1%82%D0%B0%D0%BB%D0%B8%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D1%8F%D1%82-%D0%B3%D1%80%D0%B0%D0%B4-%D0%B2-%D0%BA%D0%BE%D0%B9%D1%82%D0%BE-%D0%B6%D0%B8%D0%B2%D0%BE%D1%82%D1%8A%D1%82-%D0%BF%D1%80%D0%B5%D0%B7-%D0%B7%D0%B8%D0%BC%D0%B0%D1%82%D0%B0-%D0%B5-%D0%BD%D0%B0%D0%B9-%D1%81%D0%BB%D0%B0%D0%B4%D1%8A%D0%BA-%D1%81%D0%BD%D0%B8%D0%BC%D0%BA%D0%B8/",
+        "https://nova.bg/news/view/2026/02/12/526884/%D0%B1%D1%80%D0%B8%D1%82%D0%BD%D0%B8-%D1%81%D0%BF%D0%B8%D1%8A%D1%80%D1%81-%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B2%D0%B0-%D0%BF%D1%80%D0%B0%D0%B2%D0%B0%D1%82%D0%B0-%D0%B2%D1%8A%D1%80%D1%85%D1%83-%D1%86%D0%B5%D0%BB%D0%B8%D1%8F-%D1%81%D0%B8-%D0%BC%D1%83%D0%B7%D0%B8%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD-%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3/",
+        "https://nova.bg/news/view/2026/02/12/527050/%D1%80%D1%83%D0%BC%D1%8A%D0%BD%D0%B8%D1%8F-%D0%BE%D1%89%D0%B5-%D0%BD%D0%B5-%D0%B5-%D1%80%D0%B5%D1%88%D0%B8%D0%BB%D0%B0-%D0%B4%D0%B0%D0%BB%D0%B8-%D0%B4%D0%B0-%D1%81%D0%B5-%D0%BF%D1%80%D0%B8%D1%81%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8-%D0%BA%D1%8A%D0%BC-%D1%81%D1%8A%D0%B2%D0%B5%D1%82%D0%B0-%D0%B7%D0%B0-%D0%BC%D0%B8%D1%80-%D0%BD%D0%B0-%D1%82%D1%80%D1%8A%D0%BC%D0%BF/",
+        "https://nova.bg/news/view/2026/02/12/526869/%D0%BF%D1%80%D0%B8%D0%BD%D1%86-%D1%83%D0%B8%D0%BB%D1%8F%D0%BC-%D0%B7%D0%B0%D1%81%D0%B0%D0%B4%D0%B8-%D0%B4%D1%80%D1%8A%D0%B2%D1%87%D0%B5-%D0%B2-%D0%BF%D1%83%D1%81%D1%82%D0%B8%D0%BD%D1%8F%D1%82%D0%B0-%D0%B2-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BD%D0%B8%D1%8F-%D0%B4%D0%B5%D0%BD-%D0%BE%D1%82-%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D0%B0%D1%82%D0%B0-%D1%81%D0%B8-%D0%B2-%D1%80%D0%B8%D1%8F%D0%B4-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE/",
     ],
 }
 
